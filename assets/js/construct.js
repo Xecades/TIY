fa8fb2e5cea03ba98b53939e7d7cd7278116f122
Xecades <i@xecades.xyz>
2021-09-29 22:37:43 +0800

    jsdelivr

--- assets/js/construct.js
+++ assets/js/construct.js
@@ -9,9 +9,9 @@
     await loadJS("https://cdn.jsdelivr.net/npm/ace-builds@1.4.12/src-min-noconflict/ext-language_tools.js");
     await loadJS("https://cdn.jsdelivr.net/npm/ace-builds@1.4.12/src-min-noconflict/ext-themelist.js");
 
-    await loadJS("https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.13.0/beautify.min.js");
-    await loadJS("https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.13.0/beautify-css.min.js");
-    await loadJS("https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.13.0/beautify-html.min.js");
+    await loadJS("https://cdn.jsdelivr.net/npm/js-beautify@1.14.0/js/lib/beautifier.min.js");
+    await loadJS("https://cdn.jsdelivr.net/npm/js-beautify@1.14.0/js/lib/beautify-css.min.js");
+    await loadJS("https://cdn.jsdelivr.net/npm/js-beautify@1.14.0/js/lib/beautify-html.min.js");
 
     await loadJS("assets/js/content.js");
     await loadJS("assets/js/script.js");
